1521b286af52323c8b691cce5f712331480b8ed0
shanghuifu <shanghuifu@gmail.com>
2019-10-24 20:15:19 +0900

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  resources :tweets, only: [:index, :new, :create]
+  devise_for :users
+  root to: "tweets#index"
+  
+  namespace :tweets do
+    resources :searches, only: :index
+  end
+
+  resources :tweets do
+    resources :comments, only: :create
+  end
+  
+  resources :users, only: :show
 end
