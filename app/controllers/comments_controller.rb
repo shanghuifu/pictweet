345d5bc4b4aceaba90836dd68ba46d3e03b66671
shanghuifu <shanghuifu@gmail.com>
2019-11-01 20:49:27 +0900

    非同期通信
    404エラー。
    理由は、$.ajaxで定義したurlが存在しないこと。message#searchは未実装。
    コードのみ記述。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,14 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(comment_params)
-    redirect_to "/tweets/#{comment.tweet.id}"
+    @comment = Comment.create(comment_params)
+    respond_to do |format|
+      format.html {redirect_to tweet_path(params[:tweet_id])}
+      format.json
+    end
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
+    params.permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
   end
 end
